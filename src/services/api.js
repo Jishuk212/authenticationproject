2e675e8621513feaed2ac0f04228980dd2281af1
Jishuk212 <jishuk212@gmail.com>
2026-09-03 13:26:56 +0530

    Update API base URL for authentication service Frontend api.js base url changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://authentication-project-2utt.onrender.com/api",
+  baseURL: "https://authentication-project-1-apyd.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
